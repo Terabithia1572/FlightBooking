ce20ea34f798139f4457641ec1f6b8af386b7326
Yunus INAN <legendys65@gmail.com>
2026-09-12 23:12:40 +0300

    4# Creating Topbar and Navbar View Components

---
+++ FlightBooking/ViewComponents/DefaultViewComponents/_DefaultNavbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultNavbarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() // Bu metot, ViewComponent'ın render edilmesi sırasında çağrılır ve ViewComponent'ın görünümünü döndürür.
+        {
+            return View(); // Bu satır, ViewComponent'ın görünümünü döndürür. ViewComponent'ın görünümü, Views/Shared/Components/_DefaultNavbarComponentPartial/Default.cshtml dosyasında bulunur.
+        }
+    }
+}
