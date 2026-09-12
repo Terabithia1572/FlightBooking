5df57e24c8aa40b49a92fee8e31ae8ec3f7062bc
Yunus INAN <legendys65@gmail.com>
2026-09-12 22:57:03 +0300

    3# Creating the Layout Structure with View Components

---
+++ FlightBooking/ViewComponents/DefaultViewComponents/_DefaultHeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultHeadComponentPartial: ViewComponent // Bu sınıf, ViewComponent'ı temsil eder ve ViewComponent'ın görünümünü döndürmek için Invoke() metodunu içerir.
+    {
+        public IViewComponentResult Invoke() // Bu metot, ViewComponent'ın render edilmesi sırasında çağrılır ve ViewComponent'ın görünümünü döndürür.
+        {
+            return View(); // Bu satır, ViewComponent'ın görünümünü döndürür. ViewComponent'ın görünümü, Views/Shared/Components/_DefaultHeadComponentPartial/Default.cshtml dosyasında bulunur.
+        }
+    }
+}
---
+++ FlightBooking/ViewComponents/DefaultViewComponents/_DefaultHeaderComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultHeaderComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() // Bu metot, ViewComponent'ın render edilmesi sırasında çağrılır ve ViewComponent'ın görünümünü döndürür.
+        { 
+            return View(); // Bu satır, ViewComponent'ın görünümünü döndürür. ViewComponent'ın görünümü, Views/Shared/Components/_DefaultHeaderComponentPartial/Default.cshtml dosyasında bulunur.
+        }
+    }
+}
