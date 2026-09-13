cefb2da20ba455bdf2cd4726b365a7f3e1ec6363
Yunus INAN <legendys65@gmail.com>
2026-09-13 13:07:20 +0300

    5# Creating the Homepage Slider Area

---
+++ FlightBooking/ViewComponents/BookingViewComponents/_BookingTagsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.BookingViewComponents
+{
+    public class _BookingTagsComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() // Bu metod, ViewComponent'ın render edilmesi sırasında çağrılır ve ViewComponent'ın görünümünü döndürür.
+        {
+            return View(); // ViewComponent'ın görünümünü döndürür. Bu görünüm, Views/Shared/Components/_BookingTagsComponentPartial/Default.cshtml dosyasında bulunur.
+        }
+    }
+}
---
+++ FlightBooking/ViewComponents/DefaultViewComponents/_DefaultSliderComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultSliderComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() // Bu metod, view component'ın render edilmesi sırasında çağrılır ve view component'ın görünümünü döndürür.
+        {
+            return View(); // ViewComponent'ın varsayılan görünümünü döndürür. Bu, "Views/Shared/Components/_DefaultSliderComponentPartial/Default.cshtml" dosyasında bulunan Razor görünümünü temsil eder.
+        }
+    }
+}
