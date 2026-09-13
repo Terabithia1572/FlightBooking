80c794771b3093859c6fb4268cd4ef3ff23f67d3
Yunus INAN <legendys65@gmail.com>
2026-09-13 14:26:38 +0300

    13# Footer Section Design

---
+++ FlightBooking/ViewComponents/DefaultViewComponents/_DefaultFooterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultFooterComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() // Bu metod, ViewComponent'ın render edilmesi sırasında çağrılır ve ViewComponent'ın görünümünü döndürür.
+        {
+            return View(); // ViewComponent'ın görünümünü döndürür. Bu görünüm, Views/Shared/Components/_DefaultFooterComponentPartial/Default.cshtml dosyasında bulunur.
+        }
+    }
+}
