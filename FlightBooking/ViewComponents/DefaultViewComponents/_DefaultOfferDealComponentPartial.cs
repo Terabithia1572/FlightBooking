b0fada4d49f2c8f07a1bf6c77f2a2a8bc934eae7
Yunus INAN <legendys65@gmail.com>
2026-09-13 13:36:29 +0300

    8# Creation of the Most Popular Flight Deals Section

---
+++ FlightBooking/ViewComponents/DefaultViewComponents/_DefaultOfferDealComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultOfferDealComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() // Bu metod, ViewComponent'ın çalıştırıldığında çağrılacak olan ana metodudur. Bu metod, ViewComponent'ın görünümünü döndürür.
+        {
+            return View(); // Bu satır, ViewComponent'ın görünümünü döndürür. View() metodu, varsayılan olarak Views/Shared/Components/_DefaultOfferDealComponentPartial/Default.cshtml dosyasını arar ve bu dosyayı döndürür.
+        }
+    }
+}
