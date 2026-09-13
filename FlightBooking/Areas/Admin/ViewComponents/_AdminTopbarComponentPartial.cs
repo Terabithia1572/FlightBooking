8bf5c4b19793655862d8c8357a10781ce897e38b
Yunus INAN <legendys65@gmail.com>
2026-09-13 19:00:40 +0300

    17# Admin Sidebar Localization & Layout Completion

---
+++ FlightBooking/Areas/Admin/ViewComponents/_AdminTopbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.Areas.Admin.ViewComponents
+{
+    public class _AdminTopbarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() // Bu metod, ViewComponent'ın render edilmesi sırasında çağrılır ve ViewComponent'ın görünümünü döndürür.
+        {
+            return View(); // ViewComponent'ın görünümünü döndürür. Bu görünüm, Views/Shared/Components/_AdminTopbarComponentPartial/Default.cshtml dosyasında bulunur.
+        }
+    }
+}
