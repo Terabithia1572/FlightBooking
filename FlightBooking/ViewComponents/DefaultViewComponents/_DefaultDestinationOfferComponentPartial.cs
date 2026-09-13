73972c2cf59fe82dc6c2b5c10fdfee0541ace695
Yunus INAN <legendys65@gmail.com>
2026-09-13 13:56:41 +0300

    9# Popular Flights and Destination Cards Section

---
+++ FlightBooking/ViewComponents/DefaultViewComponents/_DefaultDestinationOfferComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultDestinationOfferComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() // Bu metod, ViewComponent'ın çalıştırıldığında çağrılacak olan ana metodudur. Bu metod, ViewComponent'ın görünümünü döndürür.
+        {
+            return View(); // Bu satır, ViewComponent'ın görünümünü döndürür. View() metodu, varsayılan olarak Views/Shared/Components/_DefaultDestinationOfferComponentPartial/Default.cshtml dosyasını arar ve bu dosyayı döndürür.
+        }
+    }
+}
