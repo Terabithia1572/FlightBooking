77e8bc1bd2695867293464896267f5bae6d6995b
Yunus INAN <legendys65@gmail.com>
2026-09-13 14:22:50 +0300

    12# Frequently Asked Questions (FAQ) Section Design

---
+++ FlightBooking/ViewComponents/DefaultViewComponents/_DefaultFrequantlyAskedQuestionComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultFrequantlyAskedQuestionComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() // Bu metot, view component'ın render edilmesi sırasında çağrılır ve view component'ın görünümünü döndürür.
+        {
+            return View(); // ViewComponent'ın görünümünü döndürür. Bu, view component'ın Razor view dosyasını render eder ve kullanıcıya gösterir. 
+        }
+    }
+}
---
+++ FlightBooking/ViewComponents/DefaultViewComponents/_DefaultScriptsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultScriptsComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() // Bu metod, ViewComponent'ın render edilmesi sırasında çağrılır ve ViewComponent'ın görünümünü döndürür.
+        {
+            return View(); // ViewComponent'ın görünümünü döndürür. Bu görünüm, Views/Shared/Components/_DefaultScriptsComponentPartial/Default.cshtml dosyasında bulunur.
+        }
+    }
+}
