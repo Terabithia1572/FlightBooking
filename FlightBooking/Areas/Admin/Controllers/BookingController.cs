78a04f4a4d2365483cb36b29ae6063417c818b73
Yunus INAN <legendys65@gmail.com>
2026-09-15 20:39:58 +0300

    28# Create Booking & Passenger Page (Claude AI)

---
+++ FlightBooking/Areas/Admin/Controllers/BookingController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.Areas.Admin.Controllers
+{
+    [Area("Admin")] // Bu controller'ın Admin Area alanına ait olduğunu belirtir
+    public class BookingController : Controller
+    {
+        public IActionResult CreateBooking()
+        {
+            return View();
+        }
+    }
+}
