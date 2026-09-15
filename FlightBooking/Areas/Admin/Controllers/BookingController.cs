ea87bba582af8f63eb8ae7fc4d0b803d92b1acaa
Yunus INAN <legendys65@gmail.com>
2026-09-15 21:11:22 +0300

    30# Moving the Selected Flight to the Reservation Page

--- FlightBooking/Areas/Admin/Controllers/BookingController.cs
+++ FlightBooking/Areas/Admin/Controllers/BookingController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FlightBooking.Services.FlightServices;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FlightBooking.Areas.Admin.Controllers
 {
     [Area("Admin")] // Bu controller'ın Admin Area alanına ait olduğunu belirtir
     public class BookingController : Controller
     {
-        public IActionResult CreateBooking() // CreateBooking sayfasını döndürür 
+        private readonly IFlightService _flightService;
+        public async Task< IActionResult> CreateBooking( string id) // CreateBooking sayfasını döndürür 
         {
+            var values= await _flightService.GetFlightByIdAsync(id); // FlightService üzerinden uçuş bilgilerini alır
+            ViewBag.id = id;
+            ViewBag.FlightNumber = values.FlightNumber;
+            ViewBag.DepartureAirportCode = values.DepartureAirportCode;
+            ViewBag.DepartureAirportName = values.DepartureAirportName;
+            ViewBag.ArrivalAirportCode = values.ArrivalAirportCode;
+            ViewBag.ArrivalAirportName = values.ArrivalAirportName;
+            ViewBag.DepartureTime = values.DepartureTime;
+            ViewBag.ArrivalTime = values.ArrivalTime;
+            ViewBag.AirlineCode = values.AirlineCode;
             return View();
         }
         public IActionResult BookingList() // BookingList sayfasını döndürür
