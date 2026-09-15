35a29718a94e6f8beea33efe20d2e6d5d302cc8f
Yunus INAN <legendys65@gmail.com>
2026-09-15 20:46:14 +0300

    29# Booking Scenario & Flight Selection (Admin Panel)

--- FlightBooking/Areas/Admin/Controllers/BookingController.cs
+++ FlightBooking/Areas/Admin/Controllers/BookingController.cs
@@ -5,7 +5,11 @@ namespace FlightBooking.Areas.Admin.Controllers
     [Area("Admin")] // Bu controller'ın Admin Area alanına ait olduğunu belirtir
     public class BookingController : Controller
     {
-        public IActionResult CreateBooking()
+        public IActionResult CreateBooking() // CreateBooking sayfasını döndürür 
+        {
+            return View();
+        }
+        public IActionResult BookingList() // BookingList sayfasını döndürür
         {
             return View();
         }
