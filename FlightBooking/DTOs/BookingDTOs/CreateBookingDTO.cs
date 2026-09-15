47f7df2defdd0461f9a67ad14bc5adb2d3fa2604
Yunus INAN <legendys65@gmail.com>
2026-09-15 20:36:06 +0300

    27# Flight & Passenger Entity and DTO Design

---
+++ FlightBooking/DTOs/BookingDTOs/CreateBookingDTO.cs
@@ -0,0 +1,14 @@
+﻿using FlightBooking.DTOs.PassengerDTOs;
+
+namespace FlightBooking.DTOs.BookingDTOs
+{
+    public class CreateBookingDTO
+    {
+        public string FlightId { get; set; }
+        public List<CreatePassengerDTO> Passengers { get; set; }
+        public string ContactName { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactPhone { get; set; }
+        public string PnrNumber { get; set; }
+    }
+}
