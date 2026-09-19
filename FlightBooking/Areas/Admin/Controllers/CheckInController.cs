36f4f407b700e4842cf21098469efdb03f902ee1
Yunus INAN <legendys65@gmail.com>
2026-09-19 23:55:22 +0300

    39# Check-in UI Design (Baggage, Seat, Meal, etc.)

---
+++ FlightBooking/Areas/Admin/Controllers/CheckInController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.Areas.Admin.Controllers
+{
+    public class CheckInController : Controller
+    {
+        [Area("Admin")] // Bu area , Admin alanına ait olduğunu belirtir
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
