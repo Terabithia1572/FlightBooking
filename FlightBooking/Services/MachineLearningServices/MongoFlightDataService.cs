875532723bfcda04ef0a73a4282980de6cec0204
Yunus INAN <legendys65@gmail.com>
2026-09-20 17:28:58 +0300

    60# Flight Regression Class and Controller Operations

--- FlightBooking/Services/MachineLearningServices/MongoFlightDataService.cs
+++ FlightBooking/Services/MachineLearningServices/MongoFlightDataService.cs
@@ -1,4 +1,5 @@
 ﻿using FlightBooking.MachineLearningModels;
+using FlightBooking.MachineLearningRegressionModels;
 using FlightBooking.Settings;
 using MongoDB.Driver;
 
@@ -36,6 +37,22 @@ namespace FlightBooking.Services.MachineLearningServices
             return mlData;
         }
 
-     
+        public async Task<List<FlightRegressionData>> ConvertToRegressionDataAsync()
+        {
+            var rawData = await GetAllAsync();
+
+            var regressionData = rawData.Select(x => new FlightRegressionData
+            {
+                Month = DateTime.Parse(x.FlightDate).Month,
+
+                DayOfWeek = (float)DateTime.Parse(x.FlightDate).DayOfWeek,
+
+                FlightType = x.FlightType == "Morning" ? 0 : 1,
+
+                PassengerCount = x.PassengerCount
+            }).ToList();
+
+            return regressionData;
+        }
     }
 }
