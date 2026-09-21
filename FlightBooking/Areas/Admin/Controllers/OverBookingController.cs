255f9e8316b1bb64e115cf18468a5bef32213f8a
Yunus INAN <legendys65@gmail.com>
2026-09-21 20:52:53 +0300

    67# AI Risk Intelligence Dashboard and Operations Panel

---
+++ FlightBooking/Areas/Admin/Controllers/OverBookingController.cs
@@ -0,0 +1,57 @@
+﻿using FlightBooking.DTOs.NoShowDTOs;
+using FlightBooking.Services.NoShowServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OverBookingController : Controller
+    {
+        private readonly NoShowService
+            _mongoNoShowService;
+
+        private readonly OverbookingRecommendationService
+            _overbookingRecommendationService;
+
+        public OverBookingController(
+            NoShowService mongoNoShowService,
+            OverbookingRecommendationService
+                overbookingRecommendationService)
+        {
+            _mongoNoShowService = mongoNoShowService;
+
+            _overbookingRecommendationService =
+                overbookingRecommendationService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var flights =
+                await _mongoNoShowService.GetAllAsync();
+
+            var results = new List<dynamic>();
+
+            foreach (var flight in flights)
+            {
+                var recommendation =
+                    await _overbookingRecommendationService
+                        .GenerateRecommendationAsync(
+                            flightDate:
+                                flight.FlightDate,
+
+                            flightSlot:
+                                flight.FlightSlot,
+
+                            passengerCount:
+                                flight.BoardedPassenger,
+
+                            capacity:
+                                flight.Capacity);
+
+                results.Add(recommendation);
+            }
+
+            return View(results);
+        }
+    }
+}
