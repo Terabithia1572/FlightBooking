5d61d95ab05b9518d6645af99a7b9dfdaa027b47
Yunus INAN <legendys65@gmail.com>
2026-09-21 21:14:54 +0300

    71# Overbooking and Passenger Forecasting with ML.NET

--- FlightBooking/Program.cs
+++ FlightBooking/Program.cs
@@ -3,6 +3,7 @@ using FlightBooking.Services.CheckInServices;
 using FlightBooking.Services.FlightServices;
 using FlightBooking.Services.MachineLearningServices;
 using FlightBooking.Services.NoShowServices;
+using FlightBooking.Services.OverBookingNoShowServices;
 using FlightBooking.Settings;
 using Microsoft.Extensions.Options;
 using System.Reflection;
@@ -17,6 +18,7 @@ builder.Services.AddSingleton<FlightRegressionService>(); // FlightRegressionSer
 builder.Services.AddScoped<MongoFlightDataService>(); // MongoFlightDataService sýnýfýný DI konteynerine ekledik.
 builder.Services.AddScoped<NoShowService>(); // NoShowService sýnýfýný DI konteynerine ekledik.
 builder.Services.AddScoped<OverbookingRecommendationService>(); // OverbookingRecommendationService sýnýfýný DI konteynerine ekledik.
+builder.Services.AddScoped<NoShowPredictionService>(); // NoShowPredictionService sýnýfýný DI konteynerine ekledik.
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly()); // AutoMapper'ý DI konteynerine ekledik ve mevcut assembly'i taradýk.
 builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettingsKey")); // DatabaseSettingsKey sýnýfýný DI konteynerine ekledik ve appsettings.json dosyasýndaki DatabaseSettings bölümünü bind ettik.
 builder.Services.AddScoped<IDatabaseSettings>(sp => {
