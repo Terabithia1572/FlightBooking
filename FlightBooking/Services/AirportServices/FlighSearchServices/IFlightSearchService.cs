15fcda167294609a795d519c44019090ed72c2e0
Yunus INAN <legendys65@gmail.com>
2026-09-24 20:09:40 +0300

    94# Writing Models for the Flight List

---
+++ FlightBooking/Services/AirportServices/FlighSearchServices/IFlightSearchService.cs
@@ -0,0 +1,9 @@
+﻿using FlightBooking.DTOs.FlightSearchDTOs;
+
+namespace FlightBooking.Services.AirportServices.FlighSearchServices
+{
+    public interface IFlightSearchService
+    {
+        Task<List<FlightCardDTO>> SearchAsync(string fromIata, string toIata, string outboundDate, int adults, string cabin, string currency);
+    }
+}
